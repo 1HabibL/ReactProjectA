e9332c386cab56a748ecdbca21e402c55aa0e3c0
1HabibL <50036548+1HabibL@users.noreply.github.com>
2023-09-15 01:12:31 -0400

    Add files via upload

---
+++ ListGroup.tsx
@@ -0,0 +1,35 @@
+import { MouseEvent } from "react";
+function ListGroup() {
+  let items = [
+    "blaziken",
+    "tapubulu",
+    "KingGambit",
+    "RotomWash",
+    "scizor",
+    "hatterene",
+  ];
+  items = [];
+
+ //event handler
+  const handleClick = (event: MouseEvent) => console.log(event.)
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
